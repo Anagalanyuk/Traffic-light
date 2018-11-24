3b58c1ffaf6ca02649bfbf95b2d6af2d49a189ae
Anatoliy Anatoliy <anagalanyuk@gmail.com>
2018-11-24 02:19:08 +0200

    update YelowLight

--- Traffic light/Yalowlight.h
+++ Traffic light/Yalowlight.h
@@ -11,35 +11,39 @@ class YelowLight: public BaseState, public IState
 public:
 	virtual void Update(TrafficLight* traffic)override
 	{
-		offSet = 1;
-		width = 5;
-		point.X = x;
-		point.Y = y + 6;
-
-		SetConsoleTextAttribute(handle, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY);
-		for (int i = 0; i < 5; ++i)
+		for (int i = 0; i < 4; ++i)
 		{
-			for (int j = 0; j < width; ++j)
+			offSet = 1;
+			width = 5;
+			point.X = x;
+			point.Y = y + 6;
+
+			SetConsoleTextAttribute(handle, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY);
+			for (int i = 0; i < 5; ++i)
 			{
-				SetConsoleCursorPosition(handle, point);
-				std::cout << cell << std::endl;
-				point.X++;
+				for (int j = 0; j < width; ++j)
+				{
+					SetConsoleCursorPosition(handle, point);
+					std::cout << cell << std::endl;
+					point.X++;
+				}
+				if (i < 2)
+				{
+					width += 2;
+					point.X = x - i - 1;
+				}
+				else
+				{
+					width -= 2;
+					point.X = x - offSet;
+					offSet--;
+				}
+				point.Y++;
 			}
-			if (i < 2)
-			{
-				width += 2;
-				point.X = x - i - 1;
-			}
-			else
-			{
-				width -= 2;
-				point.X = x - offSet;
-				offSet--;
-			}
-			point.Y++;
+			Sleep(500);
+			DeleteState();
+			Sleep(500);
 		}
-		Sleep(4000);
-		DeleteState();
 		traffic->SetState(new GreenLight);
 
 	}
