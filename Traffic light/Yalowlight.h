4e3765f6b0c1644b67fd5b2a89efe9ad3bb5e7e9
Anatoliy Anatoliy <anagalanyuk@gmail.com>
2018-11-24 23:29:03 +0200

    Revert "update YelowLight"
    This reverts commit 3b58c1ffaf6ca02649bfbf95b2d6af2d49a189ae.

--- Traffic light/Yalowlight.h
+++ Traffic light/Yalowlight.h
@@ -11,39 +11,35 @@ class YelowLight: public BaseState, public IState
 public:
 	virtual void Update(TrafficLight* traffic)override
 	{
-		for (int i = 0; i < 4; ++i)
-		{
-			offSet = 1;
-			width = 5;
-			point.X = x;
-			point.Y = y + 6;
+		offSet = 1;
+		width = 5;
+		point.X = x;
+		point.Y = y + 6;
 
-			SetConsoleTextAttribute(handle, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY);
-			for (int i = 0; i < 5; ++i)
+		SetConsoleTextAttribute(handle, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY);
+		for (int i = 0; i < 5; ++i)
+		{
+			for (int j = 0; j < width; ++j)
 			{
-				for (int j = 0; j < width; ++j)
-				{
-					SetConsoleCursorPosition(handle, point);
-					std::cout << cell << std::endl;
-					point.X++;
-				}
-				if (i < 2)
-				{
-					width += 2;
-					point.X = x - i - 1;
-				}
-				else
-				{
-					width -= 2;
-					point.X = x - offSet;
-					offSet--;
-				}
-				point.Y++;
+				SetConsoleCursorPosition(handle, point);
+				std::cout << cell << std::endl;
+				point.X++;
 			}
-			Sleep(500);
-			DeleteState();
-			Sleep(500);
+			if (i < 2)
+			{
+				width += 2;
+				point.X = x - i - 1;
+			}
+			else
+			{
+				width -= 2;
+				point.X = x - offSet;
+				offSet--;
+			}
+			point.Y++;
 		}
+		Sleep(4000);
+		DeleteState();
 		traffic->SetState(new GreenLight);
 
 	}
